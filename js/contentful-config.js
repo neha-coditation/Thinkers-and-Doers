5e39521aa2aa7940af057e4cf8e31b9d6cee6769
neha-coditation <neha@coditation.com>
2026-08-19 23:07:08 +0530

    Update contentful-config.js

--- js/contentful-config.js
+++ js/contentful-config.js
@@ -3,5 +3,5 @@ window.CONTENTFUL_CONFIG = {
   environment: "master",
   deliveryToken: "3a970c4a77a22a735324dc13f2964419f3f745c1ff571371b9b0977159bd7a9a",
   contentType: "episode",
-  enabled: false
+  enabled: true
 };
