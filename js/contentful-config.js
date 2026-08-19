a0d5de6e84aa2a2b3c066fe5fdb72c37e3b3bf4c
neha-coditation <neha@coditation.com>
2026-08-19 22:52:10 +0530

    Update contentful-config.js

--- js/contentful-config.js
+++ js/contentful-config.js
@@ -1,1 +1,7 @@
-window.CONTENTFUL_CONFIG={spaceId:"9pexc9p6gihi",environment:"master",deliveryToken:"3a970c4a77a22a735324dc13f2964419f3f745c1ff571371b9b0977159bd7a9a",contentType:"episode",guestContentType:"guest",enabled:false};
+window.CONTENTFUL_CONFIG = {
+  spaceId: "9pexc9p6gihi",
+  environment: "master",
+  deliveryToken: "3a970c4a77a22a735324dc13f2964419f3f745c1ff571371b9b0977159bd7a9a",
+  contentType: "episode",
+  enabled: false
+};
