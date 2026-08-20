3b361db29d8008629325e72ca459ac5cb77ee7d0
neha-coditation <neha@coditation.com>
2026-08-20 13:51:09 +0530

    Add guestContentType to contentful config

--- js/contentful-config.js
+++ js/contentful-config.js
@@ -3,5 +3,6 @@ window.CONTENTFUL_CONFIG = {
   environment: "master",
   deliveryToken: "3a970c4a77a22a735324dc13f2964419f3f745c1ff571371b9b0977159bd7a9a",
   contentType: "episode",
+  guestContentType: "guest",
   enabled: true
 };
